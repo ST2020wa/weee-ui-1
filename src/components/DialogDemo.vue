39f6d197ad2951c140a19cf349272de4a00f07fe
ST Li <kidtime@126.com>
2020-12-02 22:13:15 +0800

    let dialog support visible props

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -3,13 +3,25 @@
     Hey I'm DialogDemo!
   </div>
   <h1>Sample 1</h1>
-  <Dialog></Dialog>
+  <Button @click="toggle">toggle</Button>
+  <Dialog :visible="x"></Dialog>
 </template>
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
+import Button from '../lib/Button.vue'
+import {ref} from 'vue'
 export default {
   components: {
-    Dialog
+    Dialog, Button
+  },
+  setup(){
+    const x = ref(false)
+    const toggle = ()=>{
+      x.value = !x.value
+    }
+    return {
+      x, toggle
+    }
   }
 }
 </script>
