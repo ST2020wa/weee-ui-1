54a1c9e68ad96591bb08e1729d498da83cb80d15
ST Li <kidtime@126.com>
2020-12-02 17:04:27 +0800

    debug loading problem

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,33 @@
 <template>
-    <button class="weee-button" :class="{[`theme-${theme}`]: theme}">
+  <button class="weee-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="weee-loadingIndicator"></span>
       <slot />
     </button>
 </template>
 <script lang="ts">
+  import { computed } from 'vue';
   export default {
     props: {
       theme: {type: String, default:'button'},
+      size: {type: String, default: 'normal'},
+      level: {type: String, default: 'normal'},
+      disabled: {type: Boolean, default: false},
+      loading: {type: Boolean, default: false}
+    },
+  setup(props){
+      const {theme,size} = props
+    const classes = computed(()=>{
+      return {
+        [`weee-theme-${theme}`]: theme,
+        [`weee-size-${size}`]: size,
+      };
+    });
+      return {classes}
     }
   }
+
 </script>
-<style lang="scss" scoped>
+<style lang="scss">
 $h: 32px;
 $border-color: #d9d9d9;
 $color: #333;
@@ -30,23 +47,64 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
-
   & + & {
     margin-left: 8px;
   }
-
   &:hover,
   &:focus {
     color: $blue;
     border-color: $blue;
   }
-
   &:focus {
     outline: none;
   }
-
   &::-moz-focus-inner {
     border: 0;
   }
+  &.weee-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.weee-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);;
+    }
+  }
+  &.weee-theme-button{
+    &.weee-size-big{
+      font-size: 24px;
+      height: 48px;
+      padding: 0 16px
+    }
+    &.weee-size-small{
+      font-size: 12px;
+      height: 20px;
+      padding: 0 4px;
+    }
+  }
+  > .weee-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: weee-spin 1s infinite linear;
+  }
+}
+@keyframes weee-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
 }
 </style>
