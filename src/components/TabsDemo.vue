e73be454dd2bd45378de597f057084e421028948
ST Li <kidtime@126.com>
2020-12-06 17:36:22 +0800

    initiate tabs and tab component

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -2,4 +2,20 @@
   <div>
     Hey I'm TabsDemo!
   </div>
+  <h1>Sample 1</h1>
+  <Tabs>
+    <Tab title="Nav1"> Content 1 </Tab>
+    <Tab title="Nav2"> Content 2 </Tab>
+  </Tabs>
 </template>
+
+<script lang="ts">
+import Tabs from '../lib/Tabs.vue'
+import Tab from '../lib/Tab.vue'
+export default {
+  components: {
+    Tabs,
+    Tab
+  }
+}
+</script>
