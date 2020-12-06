d6f9604c7768d5ba23b4881fd2b3566fc596ae90
ST Li <kidtime@126.com>
2020-12-06 19:21:00 +0800

    update with one bug unable to solve

--- src/components/TabsDemo.vue
+++ src/components/TabsDemo.vue
@@ -3,7 +3,7 @@
     Hey I'm TabsDemo!
   </div>
   <h1>Sample 1</h1>
-  <Tabs>
+  <Tabs v-model:selected="x">
     <Tab title="Nav1"> Content 1 </Tab>
     <Tab title="Nav2"> Content 2 </Tab>
   </Tabs>
@@ -12,10 +12,15 @@
 <script lang="ts">
 import Tabs from '../lib/Tabs.vue'
 import Tab from '../lib/Tab.vue'
+import {ref} from 'vue'
 export default {
   components: {
     Tabs,
     Tab
+  },
+  setup(){
+    const x = ref('Nav1')
+    return {x}
   }
 }
 </script>
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,12 +1,27 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-    <component v-for="(c,index) in defaults" :is="c" :key="index" />
+  <div class="weee-tabs">
+    <div class="weee-tabs-nav">
+      <div class="weee-tabs-nav-item"
+           @click="select(t)"
+           :class="{selected: t=== selected}"
+           v-for="(t,index) in titles"
+           :key="index">{{t}}</div>
+    </div>
+    <div class="weee-tabs-content">
+      <component class="weee-tabs-content-item"
+                 :is="current" />
+    </div>
   </div>
 </template>
 <script lang="ts">
 import Tab from './Tab.vue'
+import {computed} from 'vue'
 export default {
+  props: {
+    selected: {
+      type:String
+    }
+  },
   setup(props, context){
     const defaults = context.slots.default()
     defaults.forEach((tag)=>{
@@ -14,10 +29,45 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab～')
       }
     })
+    const current = computed(()=>{
+      return defaults.filter((tag)=>{
+        return tag.props.title === props.selected
+      })[0]
+    })
     const titles = defaults.map((tag)=>{
       return tag.props.title
     })
-    return {defaults, titles}
+    const select = (title: string)=>{
+      context.emit('update:selected', title)
+    }
+    return {defaults, titles, current, select}
   }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.weee-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: cell;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
