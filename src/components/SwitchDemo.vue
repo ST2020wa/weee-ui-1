fdf60fd8fb98d5001d9c1c18b3f07fc68039e52f
ST Li <kidtime@126.com>
2020-12-07 15:58:36 +0800

    show original code-bug exists

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,17 +1,48 @@
 <template>
   <div>
-    <Switch v-model:value="y" />
-<!--    Switch有一个value，这个value是自动监听的（也可以理解为双向绑定-->
+    <h1>Switch 组件示例 </h1>
+    <div class="demo">
+      <h2>常规用法</h2>
+      <div class="demo-component">
+        <component :is="Switch1Demo" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>{{Switch1Demo.__sourceCode}}</pre>
+      </div>
+    </div>
+    <div class="demo">
+      <h2>支持 disabled </h2>
+      <div class="demo-component">
+        <component :is="Switch2Demo" />
+      </div>
+      <div class="demo-actions">
+        <Button>查看代码</Button>
+      </div>
+      <div class="demo-code">
+        <pre>{{Switch2Demo.__sourceCode}}</pre>
+      </div>
+    </div>
   </div>
 </template>
+
 <script lang="ts">
 import Switch from '../lib/Switch.vue'
+import Button from '../lib/Button.vue'
+import Switch1Demo from './Switch1.demo.vue'
+import Switch2Demo from './Switch2.demo.vue'
 import {ref} from 'vue'
 export default {
-  components: {Switch},
+  components: {Button},
   setup(){
-    const y = ref(false)
-    return {y}
+    const bool = ref(false)
+    return {
+      bool,
+      Switch1Demo,
+      Switch2Demo
+    }
   }
 }
 </script>
