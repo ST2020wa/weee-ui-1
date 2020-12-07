b9c2dcc37bcaa03fdceb2289fe1c2c01be9964fb
ST Li <kidtime@126.com>
2020-12-07 11:18:07 +0800

    markdown update, build bug exists

--- src/views/Install.vue
+++ src/views/Install.vue
@@ -1,13 +1,15 @@
 <template>
-<article class="markdown-body">
-  <h1>Install</h1>
-  <p>Open terminal and run as follows:</p>
-  <pre><code>npm install weee-ui</code></pre>
-  <p>OR</p>
-  <pre><code>yarn add weee-ui</code></pre>
-
-  <p>
-    Next chapter: <a href="#/doc/get-started">Get Started</a>
-  </p>
-</article>
+  <article class="markdown-body" v-html="md">
+  </article>
 </template>
+
+<script>
+import md from '../mardown/install.md';
+export default {
+  data() {
+    return {
+      md
+    }
+  }
+}
+</script>
