c37a55c6c7a748490e36bd37fab8158f0ed484b8
ST Li <kidtime@126.com>
2020-12-07 13:48:21 +0800

    markdown optmization 2

--- src/router.ts
+++ src/router.ts
@@ -5,9 +5,6 @@ import SwitchDemo from "./components/SwitchDemo.vue";
 import ButtonDemo from "./components/ButtonDemo.vue";
 import DialogDemo from "./components/DialogDemo.vue";
 import TabsDemo from "./components/TabsDemo.vue";
-import GetStarted from './views/GetStarted.vue';
-import Install from './views/Install.vue';
-import Intro from './views/Intro.vue';
 
 import { h } from 'vue';
 import Markdown from './components/Markdown.vue';
