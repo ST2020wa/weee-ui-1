f6c8ee5bc6e9d12437642fae2f09db96b270df59
ST Li <kidtime@126.com>
2021-02-24 18:04:33 +0800

    ButtonComponentFinalVer

--- src/components/Button3.demo.vue
+++ src/components/Button3.demo.vue
@@ -9,18 +9,6 @@
       <Button level="safe">安全操作</Button>
       <Button level="danger">危险操作</Button>
     </div>
-<!--    <br/>-->
-<!--    <div>-->
-<!--      <Button theme="link" level="main">重要级别链接</Button>-->
-<!--      <Button theme="link">普普通通链接</Button>-->
-<!--      <Button theme="link" level="danger">危险操作链接</Button>-->
-<!--    </div>-->
-<!--    <br/>-->
-<!--    <div>-->
-<!--      <Button theme="text" level="main">重要级别文字</Button>-->
-<!--      <Button theme="text">普普通通文字</Button>-->
-<!--      <Button theme="text" level="danger">危险操作文字</Button>-->
-<!--    </div>-->
   </div>
 </template>
 
--- src/components/Button4.demo.vue
+++ src/components/Button4.demo.vue
@@ -3,8 +3,8 @@
 </demo>
 <template>
   <Button disabled>禁用按钮</Button>
-  <Button theme="link" disabled>禁用链接按钮</Button>
-  <Button theme="text" disabled>禁用按钮</Button>
+  <Button theme="link" disabled>禁用链接</Button>
+  <Button theme="text" disabled>禁用文字</Button>
 </template>
 
 <script lang="ts">
